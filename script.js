a332b00147b9064c50ed8734c75fa18819c49c9a
rifkegribenes <rifkegribenes@gmail.com>
2018-09-22 23:16:03 -0700

    cleanup

--- script.js
+++ script.js
@@ -13,8 +13,6 @@ const hexToRgb = (hex) => {
   } : null;
 }
 
-const format = d3.format(",d");
-
 
 // color scale
 const color = d3.scaleOrdinal([
@@ -37,8 +35,6 @@ d3.select("body")
 const render = (error, games) => {
   if (error) throw error;
 
-  console.log(games);
-
   // initialize treemap
   const treemap = d3.treemap()
     .tile(d3.treemapResquarify)
@@ -52,7 +48,7 @@ const render = (error, games) => {
       .style("top", `${d3.event.pageY}px`)
       .style("left", `${d3.event.pageX + 20}px`)
       .attr("data-value", () => d.data.value)
-      .html(() => `<span class="tip-name">${d.data.name}</span><br><span class="tip-mass">$${d.data.value}</span>`);
+      .html(() => `<span class="tip-name">${d.data.name}</span><br><span class="tip-mass">${d.data.value}</span>`);
   }
 
   // hide tooltip
@@ -64,16 +60,13 @@ const render = (error, games) => {
   // add game data
   const root = d3.hierarchy(games)
     .eachBefore((d) => {
-      // console.log(d);
       d.data.id = (d.parent ? `${d.parent.data.id}.` : "") + d.data.name;
-      // console.log(d.data.id);
        })
     .sum((d) => d.value)
     .sort((a, b) => b.height - a.height || b.value - a.value);
 
   // find min and max values in each category
   const catMinMax = games.children.map((category) => {
-    console.log(category);
     const min = d3.min(category.children, d => d.value);
     const max = d3.max(category.children, d => d.value);
     return {
@@ -83,8 +76,9 @@ const render = (error, games) => {
       range: d3.range(min, max, (max - min) / category.children.length)
     }
   })
-  console.log(catMinMax);
 
+  // generate alpha value for each tile
+  // smaller tiles within each category are lighter, larger tiles are darker
   const alphaScale = (catName) => {
     const min = catMinMax.find(catObj => catObj.name === catName).min
     const max = catMinMax.find(catObj => catObj.name === catName).max
@@ -105,25 +99,17 @@ const render = (error, games) => {
     .attr("class", "tile")
     .attr("width", (d) => d.x1 - d.x0)
     .attr("height", (d) => d.y1 - d.y0)
-    .attr("data-area", (d) => {
-      // console.log(`width: ${d.x1 - d.x0}`);
-      // console.log(`height: ${d.y1 - d.y0}`);
-      // console.log(`area: ${(d.x1 - d.x0)*(d.y1 - d.y0)}`);
-      // console.log(`value: ${d.data.value}`);
-      console.log(`val/area: ${d.data.value/((d.x1 - d.x0)*(d.y1 - d.y0))}`);
-    })
     .attr("fill", (d) => {
       const rgb = hexToRgb(color(d.data.category));
       rgb.a = alphaScale(d.data.category)(d.data.value);
       const { r, g, b, a } = rgb;
       return `rgba(${r},${g},${b},${a})`;
-
-      // return color(d.data.category);
     })
     .attr("data-name", (d) => d.data.name)
     .attr("data-category", (d) => d.data.category)
     .attr("data-value", (d) => d.data.value)
-        // add tooltips
+
+    // add tooltips
     .on('mouseover', (d) => showTip(d))
     .on('mouseout', () => hideTip());
 
@@ -137,50 +123,46 @@ const render = (error, games) => {
     .selectAll("tspan")
       .data((d) => d.data.name.split(/(?=[A-Z][^A-Z])/g))
     .enter().append("tspan")
-      .attr("x", 4)
-      .attr("y", (d, i) => 13 + i * 10)
+      .attr("x", 6)
+      .attr("y", (d, i) => 19 + i * 13)
+      .attr("class", "cell-label")
       .text((d) => d);
 
   // initialize legend
-
   let categories = root.leaves().map((nodes) => nodes.data.category);
-  categories = categories.filter((category, index, self) => self.indexOf(category)===index);
+  categories = categories.filter((category, index, self) => self.indexOf(category) === index);
 
   const legend = d3.select("#legend")
   const legendWidth = legend.attr("width");
-  const LEGEND_OFFSET = 10;
-  const LEGEND_RECT_SIZE = 15;
-  const LEGEND_H_SPACING = 150;
-  const LEGEND_V_SPACING = 10;
-  const LEGEND_TEXT_X_OFFSET = 3;
-  const LEGEND_TEXT_Y_OFFSET = -2;
-  const legendElemsPerRow = Math.floor(legendWidth/LEGEND_H_SPACING);
+  const legendOffset = 10;
+  const legendRectSize = 15;
+  const legendHSpace = 150;
+  const legendVSpace = 10;
+  const legendTextXOffset = 3;
+  const legendTextYOffset = -2;
+  const legendElemsPerRow = Math.floor(legendWidth / legendHSpace);
 
   const legendElem = legend
     .append("g")
-    .attr("transform", "translate(60," + LEGEND_OFFSET + ")")
+    .attr("transform", `translate(60, ${legendOffset})`)
     .selectAll("g")
     .data(categories)
     .enter().append("g")
-    .attr("transform", function(d, i) {
-      return 'translate(' +
-      ((i%legendElemsPerRow)*LEGEND_H_SPACING) + ',' +
-      ((Math.floor(i/legendElemsPerRow))*LEGEND_RECT_SIZE + (LEGEND_V_SPACING*(Math.floor(i/legendElemsPerRow)))) + ')';
+    .attr("transform", (d, i) => {
+      return `translate(${((i % legendElemsPerRow) * legendHSpace)}, ${
+      ((Math.floor(i / legendElemsPerRow)) * legendRectSize + (legendVSpace * (Math.floor(i / legendElemsPerRow))))})`;
     })
 
   legendElem.append("rect")
-     .attr('width', LEGEND_RECT_SIZE)
-     .attr('height', LEGEND_RECT_SIZE)
+     .attr('width', legendRectSize)
+     .attr('height', legendRectSize)
      .attr('class','legend-item')
-     .attr('fill', function(d){
-       return color(d);
-     })
+     .attr('fill', (d) => color(d));
 
    legendElem.append("text")
-     .attr('x', LEGEND_RECT_SIZE + LEGEND_TEXT_X_OFFSET)
-     .attr('y', LEGEND_RECT_SIZE + LEGEND_TEXT_Y_OFFSET)
-     .text(function(d) { return d; });
-
+     .attr('x', legendRectSize + legendTextXOffset)
+     .attr('y', legendRectSize + legendTextYOffset)
+     .text((d) => d);
 
 }
 
